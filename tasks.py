cd8037d4d408c4d39a6f4d5aca86d46ec84971bf
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-05-30 16:48:11 -0300

    Integration 106

--- tasks.py
+++ tasks.py
@@ -19,9 +19,7 @@ async def wait_for_paid_invoices():
         await on_invoice_paid(payment)
 
 async def wait_for_mqtt():
-    invoice_queue = asyncio.Queue()
-    register_invoice_listener(invoice_queue, "mysuperplugin")
-    asyncio.create_task(mqtt_client.connect())
+    await asyncio.create_task(mqtt_client.connect())
 
 async def on_invoice_paid(payment: Payment) -> None:
     if (
