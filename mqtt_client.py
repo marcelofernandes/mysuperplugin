7f277e4eac0f1553ca7359a8b97308d83e095c36
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-05-30 14:24:01 -0300

    Integration 91

---
+++ mqtt_client.py
@@ -0,0 +1,22 @@
+# mysuperplugin/mqtt_client.py
+
+import paho.mqtt.client as mqtt # type: ignore
+
+class MQTTClient:
+    def __init__(self, broker_url, broker_port):
+        self.client = mqtt.Client()
+        self.broker_url = broker_url
+        self.broker_port = broker_port
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+    def on_connect(self, client, userdata, flags, rc):
+        print(f"Connected to MQTT broker with result code {rc}")
+
+    def on_message(self, client, userdata, msg):
+        print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+
+    def connect(self):
+        self.client.connect(self.broker_url, self.broker_port, 60)
+        self.client.loop_start()
+
