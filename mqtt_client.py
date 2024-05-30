e28439b3e0abdf0e893ec88f62ebe7155563cae9
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-05-30 15:38:42 -0300

    Integration 100

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,5 +1,3 @@
-# mysuperplugin/mqtt_client.py
-
 import paho.mqtt.client as mqtt # type: ignore
 import asyncio
 
@@ -10,27 +8,25 @@ class MQTTClient:
         self.broker_port = broker_port
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-        self.client.on_subscribe = self.on_subscribe
 
     def on_connect(self, client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to broker")
-            # Inscrever-se em um tópico após a conexão com o broker
-            client.subscribe("test/topic")
-        else:
-            print(f"Connection failed with code {rc}")
+        print(f"Connected with result code {rc}")
+        client.subscribe("test/topic")
 
     def on_message(self, client, userdata, msg):
-        print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
-
-    def on_subscribe(self, client, userdata, mid, granted_qos):
-        print(f"Subscribed: {mid} QoS: {granted_qos}")
+        print(f"{msg.topic} {msg.payload.decode()}")
 
     async def connect(self):
         self.client.connect(self.broker_url, self.broker_port, 60)
         loop = asyncio.get_event_loop()
         await loop.run_in_executor(None, self.client.loop_forever)
 
-# Configurar o cliente MQTT
-mqtt_client = MQTTClient(broker_url="172.21.240.91", broker_port=1883)
+    def publish(self, topic, payload):
+        self.client.publish(topic, payload)
+
+# Configuração do cliente MQTT
+broker_address = "172.21.240.91"
+port = 1883
+
+mqtt_client = MQTTClient(broker_address, port)
 asyncio.run(mqtt_client.connect())
