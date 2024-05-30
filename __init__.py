027479e771b45bafad61ef7f2c6ec8f54b100e67
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-05-30 10:10:37 -0300

    Integration 76

--- __init__.py
+++ __init__.py
@@ -80,10 +80,3 @@ def mqtt_client_thread():
 mqtt_thread = threading.Thread(target=mqtt_client_thread)
 mqtt_thread.start()
 
-try:
-    while True:
-        print("Doing main application tasks...")
-        time.sleep(5)
-except KeyboardInterrupt:
-    print("Exiting main application...")
-    mqtt_thread.join()
