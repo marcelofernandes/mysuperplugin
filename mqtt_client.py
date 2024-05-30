423577f23e67d20ec78bcada5072a5e0c34a190f
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-05-30 14:52:16 -0300

    Integration 95

--- mqtt_client.py
+++ mqtt_client.py
@@ -9,16 +9,21 @@ class MQTTClient:
         self.broker_port = broker_port
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
+        self.client.on_subscribe = self.on_subscribe
 
     def on_connect(self, client, userdata, flags, rc):
-        print(f"Connected to MQTT broker with result code {rc}")
-        client.subscribe("test/topic")
-        def on_subscribe(self, client, userdata, flags, rc):
-            print(f"Subscribed to MQTT broker with result code {rc}")
-        client.on_subscribe = on_subscribe
+        if rc == 0:
+            print("Connected to broker")
+            # Inscrever-se em um tópico após a conexão com o broker
+            client.subscribe("test/topic")
+        else:
+            print(f"Connection failed with code {rc}")
 
     def on_message(self, client, userdata, msg):
         print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+
+    def on_subscribe(client, userdata, mid, granted_qos):
+        print(f"Subscribed: {mid} QoS: {granted_qos}")
 
     def connect(self):
         self.client.connect(self.broker_url, self.broker_port, 60)
