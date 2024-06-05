967b31fcf8e74322e1e2e52967e3fb8451966d9c
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-06-05 08:57:15 -0300

    Integration 165

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ from .tasks import wait_for_paid_invoices
 from .views import mysuperplugin_ext_generic
 from .views_api import mysuperplugin_ext_api
 import paho.mqtt.client as mqtt # type: ignore
+import threading
 
 db = Database("ext_mysuperplugin")
 
@@ -52,12 +53,28 @@ topic = "test/topic"
 # Callback para quando o cliente se conecta ao broker
 def on_connect(client, userdata, flags, rc):
     print("Conectado com código de retorno:", rc)
-    client.subscribe("meu/topico")
+    client.subscribe("test/topic")
 
 # Callback para quando uma mensagem é recebida do broker
 def on_message(client, userdata, msg):
     print("Mensagem recebida no tópico:", msg.topic)
     print("Payload:", msg.payload.decode())
+
+# Função para rodar o loop MQTT em um thread separado
+def start_mqtt_loop(client):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(mqtt_loop(client))
+    loop.run_forever()
+
+# Função assíncrona para conectar e iniciar o loop MQTT
+async def mqtt_loop(client):
+    client.connect(broker_address, port)
+    client.loop_start()
+
+# Configuração do broker MQTT
+broker_address = "mqtt.eclipse.org"
+port = 1883
 
 # Criação de um cliente MQTT
 client = mqtt.Client()
@@ -66,19 +83,14 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-# Função para conectar e iniciar o loop MQTT
-async def connect_and_loop():
-    client.connect(broker, port, 5)
-    client.loop_start()
-
-# Função principal assíncrona
-async def main():
-    await connect_and_loop()
+# Iniciar o loop MQTT em um thread separado
+mqtt_thread = threading.Thread(target=start_mqtt_loop, args=(client,))
+mqtt_thread.start()
 
 # Execução do loop principal asyncio
 # if __name__ == "__main__":
 #     asyncio.run(main())
-print("The name:", __name__)
+# print("The name:", __name__)
 # asyncio.run(main())
 # try:
 #     loop = asyncio.get_event_loop()
