b2eed58097c91f62a9f19126fd6fd462b4d19b55
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-06-06 15:39:06 -0300

    Integration 227

--- tasks.py
+++ tasks.py
@@ -39,11 +39,26 @@ async def print_message(message):
     print(message)
 
 async def on_message(client, userdata, msg):
-    
+
     # logger.info(f"Mensagem recebida: {msg.payload.decode()} no tópico {msg.topic}")
     # teste = test_client()
     message = f"Mensagem recebida: {msg.payload.decode()} no tópico {msg.topic}"
-    await asyncio.run_coroutine_threadsafe(print_message(message), asyncio.get_event_loop())
+    
+    try:
+        loop = asyncio.get_running_loop()
+        logger.info("Has loop")
+    except RuntimeError:  # Nenhum loop em execução
+        logger.info("Without loop")
+        loop = None
+
+    if loop and loop.is_running():
+        asyncio.create_task(print_message())
+        logger.info("Task created")
+    else:
+        logger.info("Run coroutine threadsafe")
+        asyncio.run_coroutine_threadsafe(print_message())
+
+    # asyncio.run_coroutine_threadsafe(print_message(message), loop)
     # logger.info(f"Teste message reveived: {teste}")
 
 async def example_task():
