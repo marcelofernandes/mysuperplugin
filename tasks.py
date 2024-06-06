6dbe309b33aa7bfa1028201c6c61fb6c9a085c85
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-06-06 15:13:09 -0300

    Integration 224

--- tasks.py
+++ tasks.py
@@ -34,12 +34,14 @@ def on_connect(client, userdata, flags, rc):
     logger.info("Conectado com código de resultado: " + str(rc))
     print("Connected")
     client.subscribe(topic)
+async def print_message(message):
+    print(message)
 
 def on_message(client, userdata, msg):
-    
-    logger.info(f"Mensagem recebida: {msg.payload.decode()} no tópico {msg.topic}")
-    teste = test_client()
-    print(f"Teste message reveived: {teste}")
+    # logger.info(f"Mensagem recebida: {msg.payload.decode()} no tópico {msg.topic}")
+    # teste = test_client()
+    message = f"Mensagem recebida: {msg.payload.decode()} no tópico {msg.topic}"
+    asyncio.run_coroutine_threadsafe(print_message(message), asyncio.get_event_loop())
     # logger.info(f"Teste message reveived: {teste}")
 
 async def example_task():
