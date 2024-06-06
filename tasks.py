c4aa0fef2d0a49db2cae5bb45a4f81beb6dcfab5
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-06-06 16:32:28 -0300

    Integration 237

--- tasks.py
+++ tasks.py
@@ -43,25 +43,26 @@ def on_message(client, userdata, msg):
     # logger.info(f"Mensagem recebida: {msg.payload.decode()} no tópico {msg.topic}")
     # teste = test_client()
     message = f"Mensagem recebida: {msg.payload.decode()} no tópico {msg.topic}"
-    
-    try:
-        loop = asyncio.get_running_loop()
-        logger.info("Has loop")
-    except RuntimeError:  # Nenhum loop em execução
-        logger.info("Without loop")
-        loop = None
+    asyncio.create_task(print_message(message))
+    # try:
+    #     loop = asyncio.get_running_loop()
+    #     logger.info("Has loop")
+    # except RuntimeError:  # Nenhum loop em execução
+    #     logger.info("Without loop")
+    #     loop = None
 
-    if loop and loop.is_running():
-        asyncio.create_task(print_message(message))
-        logger.info("Task created")
-    else:
-        loop = asyncio.new_event_loop()
-        # loop.run_until_complete(print_message(message))
-        asyncio.set_event_loop(loop)
-        # loop = asyncio.new_event_loop()
-        # loop.run_until_complete(print_message(message))
-        asyncio.create_task(print_message(message))
-        logger.info("Run coroutine threadsafe")
+    # if loop and loop.is_running():
+    #     asyncio.create_task(print_message(message))
+    #     logger.info("Task created")
+    # else:
+    #     loop = asyncio.new_event_loop()
+    #     # loop.run_until_complete(print_message(message))
+    #     # asyncio.set_event_loop(loop)
+    #     # loop = asyncio.new_event_loop()
+    #     # loop.run_until_complete(print_message(message))
+    #     loop.create_task(print_message(message))
+    #     loop.run_until_complete
+    #     logger.info("Run coroutine threadsafe")
 
     # asyncio.run_coroutine_threadsafe(print_message(message), loop)
     # logger.info(f"Teste message reveived: {teste}")
@@ -72,3 +73,22 @@ async def example_task():
     client.on_message = on_message
     client.connect(broker, 1883, 60)
     client.loop_start()
+    try:
+        try:
+            loop = asyncio.get_running_loop()
+            logger.info("Has loop")
+        except RuntimeError:  # Nenhum loop em execução
+            logger.info("Without loop")
+            loop = None
+
+        if loop and loop.is_running():
+            loop.run_forever()
+            logger.info("Run forever 1")
+        else:
+            loop = asyncio.new_event_loop()
+            loop.run_forever()
+            logger.info("Run forever 2")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
